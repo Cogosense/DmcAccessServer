3a268a2597e55da43489fcf568dda1f8c7392bb9
Steve Williams <steve.williams@telxio.com>
2021-06-16 07:41:58 +0000

    Fix Typo and Correct Banner Greeting

--- openvpn-configuration.sh
+++ openvpn-configuration.sh
@@ -100,7 +100,7 @@ new_client () {
 
 if [[ ! -e /etc/openvpn/server/server.conf ]]; then
     clear
-    echo 'Welcome to this OpenVPN road warrior installer!'
+    echo 'Welcome to this OpenVPN Dali DMC Access Server installer!'
     # If system has a single IPv4, it is selected automatically. Else, ask the user
     if [[ $(ip -4 addr | grep inet | grep -vEc '127(\.[0-9]{1,3}){3}') -eq 1 ]]; then
         ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}')
@@ -277,7 +277,7 @@ server 10.8.0.0 255.255.255.0" > /etc/openvpn/server/server.conf
         echo 'server-ipv6 fddd:1194:1194:1194::/64' >> /etc/openvpn/server/server.conf
         echo 'push "redirect-gateway def1 ipv6 bypass-dhcp"' >> /etc/openvpn/server/server.conf
     fi
-    echo 'idatefconfig-pool-persist ipp.txt' >> /etc/openvpn/server/server.conf
+    echo 'ifconfig-pool-persist ipp.txt' >> /etc/openvpn/server/server.conf
     # DNS
     case "$dns" in
         1|"")
